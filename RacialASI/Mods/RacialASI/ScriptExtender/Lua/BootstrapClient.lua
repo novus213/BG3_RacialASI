879dff0b90b53d5b84eedf2a79b2f1a0e8bb8b18
Thraximundar2772 <thraximundar.baffe@gmail.com>
2024-07-16 14:30:15 +0200

    Revert "Merge branch 'main' of https://github.com/novus213/BG3_RacialASI"
    This reverts commit f80d227dcf265733fe67a5558a8a0f0602bb59ea, reversing
    changes made to f8c3e97e4dc4721dacae7d22c9fb89c005e8da1a.

--- RacialASI/Mods/RacialASI/ScriptExtender/Lua/BootstrapClient.lua
+++ RacialASI/Mods/RacialASI/ScriptExtender/Lua/BootstrapClient.lua
@@ -3,8 +3,7 @@ deps = {
     Framework_WotD_GUID     = "4806bd2a-929b-406e-b1d4-2e0e9cc23bb2", -- WOT AAsimaSE
     ASTRL_Elvens_GUID       = "66b20233-cf0a-44bb-9bcf-32c0e0b09c19", -- ASTRLElvens
     ASTRL_Tieflings_GUID    = "167b846f-0a0b-4e0c-a9d0-df448be1320a", -- ASTRLTieflings
-    Framework_GUID          = "67fbbd53-7c7d-4cfa-9409-6d737b4d92a9", -- CompatibilityFramework
-    MCM_GUID                = "755a8a72-407f-4f0d-9a33-274ac0f0b53d" -- MCM (not the TV channel ^^)
+    Framework_GUID          = "67fbbd53-7c7d-4cfa-9409-6d737b4d92a9" -- CompatibilityFramework
 }
 
 if not Ext.Mod.IsModLoaded(deps.Framework_GUID) then return end
@@ -12,13 +11,8 @@ if not Ext.Mod.IsModLoaded(deps.Framework_GUID) then return end
 DevelReady = Ext.Utils:Version() >= 17 or Ext.Debug.IsDeveloperMode()
 
 Ext.Require("Shared/_Init.lua")
-Ext.Require("Client/CleanStatsASI.lua")
-
 Ext.Require("ModInfos/_ModInfos.lua")
 Ext.Require("Client/_Init.lua")
-
-
-
 
 -- Check if the mod is loaded and load ASI
 if Ext.Mod.IsModLoaded(deps.Framework_GUID) and Ext.Mod.IsModLoaded(deps.Framework_FM_GUID) then
