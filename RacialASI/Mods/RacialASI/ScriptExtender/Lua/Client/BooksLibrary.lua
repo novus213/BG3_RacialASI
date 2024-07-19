860b9342b550f8d392dbaf300b9d61499121f2f3
Novus213 <44229417+novus213@users.noreply.github.com>
2024-07-19 11:54:58 +0200

    functions payload and table

--- RacialASI/Mods/RacialASI/ScriptExtender/Lua/Client/BooksLibrary.lua
+++ RacialASI/Mods/RacialASI/ScriptExtender/Lua/Client/BooksLibrary.lua
@@ -87,14 +87,84 @@ Legacy = {	--[[ Legacy --]]
 
 
 
+
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+local AbilityList_UUID = "b9149c8e-52c8-46e5-9cb6-fc39301c05fe"
+
+--SearchedMod = "f4361c10-b197-4490-ae30-06ce796f950e"
+StatsList = {"Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"}
+
+function tableInsertRaceStats()
+	if raceMod.Stats ~= nil then
+		for i = 1, 6 do
+			table.insert(RaceStat, "Ability(" .. StatsList[i] .. "," .. raceMod.Stats[i] .. ")")
+		end
+		raceMod.Stats = RaceStat
+		print("raceMod:", dump(raceMod))
+	end
+end
+
+
+function createPayloadRaceStats(raceMod)
+
+local payload = {
+		modGuid = raceMod.modGuid,
+		Target = raceMod.UUID,
+		FileType = "Progression"
+		}
+
+	if raceMod.Sab ~= nil then
+		payload.Function = "SelectAbilityBonus"
+		payload.Params = {
+			Guid = AbilityList_UUID,
+			Amount = tablelength(raceMod.Sab),
+			BonusType = "AbilityBonus",
+			Amounts = raceMod.Sab
+		}
+		Mods.SubclassCompatibilityFramework.Api.InsertSelectors({payload})
+	end
+
+	if raceMod.Stats ~= nil then
+		payload.Type = "Boosts"
+		payload.Strings = raceMod.Strings
+		Mods.SubclassCompatibilityFramework.Api.InsertBoosts({payload})
+	end
+	print("payload:", dump(payload))
+--return payload
+end
+
+
+
 if IgnoreAll == false then
 	for _, raceMod in pairs(RaceLibrary) do
+		RaceStat =  {}
 		if raceMod.SourceBook == nil or raceMod.SourceBook == "" then
 			if IgnoreHomebrew == false then
-				-- Delete stats
-				-- Delete sab
+				print("Le mod : " .. raceMod.Name .. " est bien dans la liste")
 				-- Add Stats
 				-- Add Sab
+				tableInsertRaceStats(raceMod)
+				createPayloadRaceStats(raceMod)
 				print("Clean the Homebrew race: " .. raceMod.Name)
 			else
 				print("Ignore cleaning: " .. raceMod.Name .. " due to IgnoreHomebrew = True")
@@ -103,8 +173,8 @@ if IgnoreAll == false then
 			if Ignore5eLimited == false then
 				for _, book in pairs(Dnd5eLimited) do
 					if book.bookRef == raceMod.SourceBook then
-						-- Delete stats
-						-- Delete sab
+						tableInsertRaceStats(raceMod)
+						createPayloadRaceStats(raceMod)
 						-- Add Stats
 						-- Add Sab
 						print("Clean the 5eLimited race: " .. raceMod.Name)
@@ -113,8 +183,8 @@ if IgnoreAll == false then
 				if Ignore5e == false then
 					for _, book in pairs(Dnd5e) do
 						if book.bookRef == raceMod.SourceBook then
-							-- Delete stats
-							-- Delete sab
+							tableInsertRaceStats(raceMod)
+							createPayloadRaceStats(raceMod)
 							-- Add Stats
 							-- Add Sab
 							print("Clean the 5e race: " .. raceMod.Name)
@@ -123,8 +193,8 @@ if IgnoreAll == false then
 					if Ignore5eExtended == false then
 						for _, book in pairs(Dnd5eExtended) do
 							if book.bookRef == raceMod.SourceBook then
-								-- Delete stats
-								-- Delete sab
+								tableInsertRaceStats(raceMod)
+								createPayloadRaceStats(raceMod)
 								-- Add Stats
 								-- Add Sab
 								print("Clean the 5eExtended race: " .. raceMod.Name)
@@ -133,8 +203,8 @@ if IgnoreAll == false then
 						if IgnoreLegacy == false then
 							for _, book in pairs(Legacy) do
 								if book.bookRef == raceMod.SourceBook then
-									-- Delete stats
-									-- Delete sab
+									tableInsertRaceStats(raceMod)
+									createPayloadRaceStats(raceMod)
 									-- Add Stats
 									-- Add Sab
 									print("Clean the Legacy race: " .. raceMod.Name)
@@ -143,8 +213,8 @@ if IgnoreAll == false then
 							if IgnoreFlavours == false then
 								for _, book in pairs(Flavours) do
 									if book.bookRef == raceMod.SourceBook then
-										-- Delete stats
-										-- Delete sab
+										tableInsertRaceStats(raceMod)
+										createPayloadRaceStats(raceMod)
 										-- Add Stats
 										-- Add Sab
 										print("Clean the Flavours race: " .. raceMod.Name)
@@ -166,8 +236,8 @@ if IgnoreAll == false then
 				print("Ignore cleaning: " .. raceMod.Name .. " due to IgnoreLimited = True")
 			end
 		end
+		RaceStat =  {}
 	end
 else
 	print("Ignore cleaning the races ASI")
 end
-
