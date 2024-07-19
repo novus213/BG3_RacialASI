3ac2f1f4776ddae0e0343bdc1bce548f72892c49
Novus213 <44229417+novus213@users.noreply.github.com>
2024-07-19 13:13:09 +0200

    add warning custom print

--- RacialASI/Mods/RacialASI/ScriptExtender/Lua/Client/5eBuilder.lua
+++ RacialASI/Mods/RacialASI/ScriptExtender/Lua/Client/5eBuilder.lua
@@ -27,7 +27,9 @@ print(IgnoreHomebrew)
 
 
 
-
+--- Constructor for dump
+---@param o table
+---@return string
 function dump(o)
    if type(o) == 'table' then
       local s = '{ '
@@ -42,6 +44,9 @@ function dump(o)
 end
 
 
+--- Constructor for tablelength
+---@param T table
+---@return Integer count
 function tablelength(T)
   local count = 0
   for _ in pairs(T) do count = count + 1 end
@@ -53,19 +58,24 @@ local AbilityList_UUID = "b9149c8e-52c8-46e5-9cb6-fc39301c05fe"
 --SearchedMod = "f4361c10-b197-4490-ae30-06ce796f950e"
 StatsList = {"Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"}
 
+
+--- Constructor for tableInsertRaceStats
+---@param raceMod table raceMod
 function tableInsertRaceStats(raceMod)
 	if raceMod.Stats ~= nil then
 		for i = 1, 6 do
 			table.insert(RaceStat, "Ability(" .. StatsList[i] .. "," .. raceMod.Stats[i] .. ")")
 		end
 		raceMod.Stats = RaceStat
-		print("raceMod:", dump(raceMod))
+        BasicWarning(string.format("raceMod ============> %s", raceMod))
+		--print("raceMod:", dump(raceMod))
 	end
 end
 
 
+--- Constructor for createPayloadRaceStats
+---@param raceMod table raceMod
 function createPayloadRaceStats(raceMod)
-
 local payload = {
 		modGuid = raceMod.modGuid,
 		Target = raceMod.UUID,
@@ -88,95 +98,112 @@ local payload = {
 		payload.Strings = raceMod.Strings
 		Mods.SubclassCompatibilityFramework.Api.InsertBoosts({payload})
 	end
-	print("payload:", dump(payload))
---return payload
+    BasicWarning(string.format("payload ============> %s", payload))
+	--print("payload:", dump(payload))
+    --return payload
 end
 
-
-
-if IgnoreAll == false then
-	for _, raceMod in pairs(RaceLibrary) do
-		RaceStat =  {}
-		if raceMod.SourceBook == nil or raceMod.SourceBook == "" then
-			if IgnoreHomebrew == false then
-				print("Le mod : " .. raceMod.Name .. " est bien dans la liste")
-				-- Add Stats
-				-- Add Sab
-				tableInsertRaceStats(raceMod)
-				createPayloadRaceStats(raceMod)
-				print("Clean the Homebrew race: " .. raceMod.Name)
-			else
-				print("Ignore cleaning: " .. raceMod.Name .. " due to IgnoreHomebrew = True")
-			end
-		else
-			if Ignore5eLimited == false then
-				for _, book in pairs(Dnd5eLimited) do
-					if book.bookRef == raceMod.SourceBook then
-						tableInsertRaceStats(raceMod)
-						createPayloadRaceStats(raceMod)
-						-- Add Stats
-						-- Add Sab
-						print("Clean the 5eLimited race: " .. raceMod.Name)
-					end
-				end
-				if Ignore5e == false then
-					for _, book in pairs(Dnd5e) do
-						if book.bookRef == raceMod.SourceBook then
-							tableInsertRaceStats(raceMod)
-							createPayloadRaceStats(raceMod)
-							-- Add Stats
-							-- Add Sab
-							print("Clean the 5e race: " .. raceMod.Name)
-						end
-					end
-					if Ignore5eExtended == false then
-						for _, book in pairs(Dnd5eExtended) do
-							if book.bookRef == raceMod.SourceBook then
-								tableInsertRaceStats(raceMod)
-								createPayloadRaceStats(raceMod)
-								-- Add Stats
-								-- Add Sab
-								print("Clean the 5eExtended race: " .. raceMod.Name)
-							end
-						end
-						if IgnoreLegacy == false then
-							for _, book in pairs(Legacy) do
-								if book.bookRef == raceMod.SourceBook then
-									tableInsertRaceStats(raceMod)
-									createPayloadRaceStats(raceMod)
-									-- Add Stats
-									-- Add Sab
-									print("Clean the Legacy race: " .. raceMod.Name)
-								end
-							end
-							if IgnoreFlavours == false then
-								for _, book in pairs(Flavours) do
-									if book.bookRef == raceMod.SourceBook then
-										tableInsertRaceStats(raceMod)
-										createPayloadRaceStats(raceMod)
-										-- Add Stats
-										-- Add Sab
-										print("Clean the Flavours race: " .. raceMod.Name)
-									end
-								end
-							else
-								print("Ignore cleaning: " .. raceMod.Name .. " due to IgnoreFlavours = True")
-							end
-						else
-							print("Ignore cleaning: " .. raceMod.Name .. " due to IgnoreLegacy = True")
-						end
-					else
-						print("Ignore cleaning: " .. raceMod.Name .. " due to Ignore5eExtended = True")
-					end
-				else
-					print("Ignore cleaning: " .. raceMod.Name .. " due to Ignore5e = True")
-				end
-			else
-				print("Ignore cleaning: " .. raceMod.Name .. " due to IgnoreLimited = True")
-			end
-		end
-		RaceStat =  {}
-	end
-else
-	print("Ignore cleaning the races ASI")
+--- Constructor for builder5e
+function builder5e()
+    if IgnoreAll == false then
+        for _, raceMod in pairs(RaceLibrary) do
+            RaceStat =  {}
+            if raceMod.SourceBook == nil or raceMod.SourceBook == "" then
+                if IgnoreHomebrew == false then
+                    print("Le mod : " .. raceMod.Name .. " est bien dans la liste")
+                    -- Add Stats
+                    -- Add Sab
+                    tableInsertRaceStats(raceMod)
+                    createPayloadRaceStats(raceMod)
+                    BasicWarning(string.format("Add the Homebrew race: %s", raceMod.Name))
+                    --print("Add the Homebrew race: " .. raceMod.Name)
+                else
+                    --print("Ignore cleaning: " .. raceMod.Name .. " due to IgnoreHomebrew = True")
+                    BasicWarning(string.format("Ignore Adding: %s due to IgnoreHomebrew = True", raceMod.Name))
+                end
+            else
+                if Ignore5eLimited == false then
+                    for _, book in pairs(Dnd5eLimited) do
+                        if book.bookRef == raceMod.SourceBook then
+                            tableInsertRaceStats(raceMod)
+                            createPayloadRaceStats(raceMod)
+                            -- Add Stats
+                            -- Add Sab
+                            BasicWarning(string.format("Add the 5eLimited race: %s", raceMod.Name))
+                            --print("Add the 5eLimited race: " .. raceMod.Name)
+                        end
+                    end
+                    if Ignore5e == false then
+                        for _, book in pairs(Dnd5e) do
+                            if book.bookRef == raceMod.SourceBook then
+                                tableInsertRaceStats(raceMod)
+                                createPayloadRaceStats(raceMod)
+                                -- Add Stats
+                                -- Add Sab
+                                BasicWarning(string.format("Add the 5e race: %s", raceMod.Name))
+                                --print("Add the 5e race: " .. raceMod.Name)
+                            end
+                        end
+                        if Ignore5eExtended == false then
+                            for _, book in pairs(Dnd5eExtended) do
+                                if book.bookRef == raceMod.SourceBook then
+                                    tableInsertRaceStats(raceMod)
+                                    createPayloadRaceStats(raceMod)
+                                    -- Add Stats
+                                    -- Add Sab
+                                    BasicWarning(string.format("Add the 5eExtended race: %s", raceMod.Name))
+                                    --print("Add the 5eExtended race: " .. raceMod.Name)
+                                end
+                            end
+                            if IgnoreLegacy == false then
+                                for _, book in pairs(Legacy) do
+                                    if book.bookRef == raceMod.SourceBook then
+                                        tableInsertRaceStats(raceMod)
+                                        createPayloadRaceStats(raceMod)
+                                        -- Add Stats
+                                        -- Add Sab
+                                        BasicWarning(string.format("Add the Legacy race: %s", raceMod.Name))
+                                        --print("Add the Legacy race: " .. raceMod.Name)
+                                    end
+                                end
+                                if IgnoreFlavours == false then
+                                    for _, book in pairs(Flavours) do
+                                        if book.bookRef == raceMod.SourceBook then
+                                            tableInsertRaceStats(raceMod)
+                                            createPayloadRaceStats(raceMod)
+                                            -- Add Stats
+                                            -- Add Sab
+                                            BasicWarning(string.format("Add the Flavours race: %s", raceMod.Name))
+                                            --print("Add the Flavours race: " .. raceMod.Name)
+                                        end
+                                    end
+                                else
+                                    BasicWarning(string.format("Ignore Adding: %s due to IgnoreFlavours = True", raceMod.Name))
+                                    --print("Ignore Adding: " .. raceMod.Name .. " due to IgnoreFlavours = True")
+                                end
+                            else
+                                BasicWarning(string.format("Ignore Adding: %s due to IgnoreLegacy = True", raceMod.Name))
+                                --print("Ignore Adding: " .. raceMod.Name .. " due to IgnoreLegacy = True")
+                            end
+                        else
+                            BasicWarning(string.format("Ignore Adding: %s due to Ignore5eExtended = True", raceMod.Name))
+                            --print("Ignore Adding: " .. raceMod.Name .. " due to Ignore5eExtended = True")
+                        end
+                    else
+                        BasicWarning(string.format("Ignore Adding: %s due to Ignore5e = True", raceMod.Name))
+                        --print("Ignore Adding: " .. raceMod.Name .. " due to Ignore5e = True")
+                    end
+                else
+                    BasicWarning(string.format("Ignore Adding: %s due to IgnoreLimited = True", raceMod.Name))
+                    --print("Ignore Adding: " .. raceMod.Name .. " due to IgnoreLimited = True")
+                end
+            end
+            RaceStat =  {}
+        end
+    else
+        BasicWarning("Ignore Adding the races ASI")
+        --print("Ignore Adding the races ASI")
+    end
 end
+
+Ext.Events.StatsLoaded:Subscribe(builder5e)
