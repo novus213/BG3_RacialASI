74a455c2ee8e6ba56105459bdc4523ea5574432f
Thraximundar2772 <thraximundar.baffe@gmail.com>
2024-07-20 14:41:05 +0200

    Create ClassesLibrary.lua

---
+++ RacialASI/Mods/RacialASI/ScriptExtender/Lua/Libs/ClassesLibrary.lua
@@ -0,0 +1,380 @@
+RaceLibrary = {
+
+--  Artificer Class
+    {
+		Name 			=	"Artificer - main",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/1779",
+		modGuid 		=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"KiderionLykon",
+		SourceBook		=	"TCoE",
+		MainClasse		=	true,
+		isLvl20			=	true,
+		isOutdated		=	false
+    },
+    {
+		Name			=	"Artificer - Alchemist",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/1779",
+		modGuid 		=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"KiderionLykon",
+		SourceBook		=	"TCoE",
+		MainClasse		=	false,
+		isLvl20			=	true,
+		isOutdated		=	false
+    },
+    {
+		Name 			=	"Artificer - Armorer",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/1779",
+		modGuid 		=	"aaaa",
+		UUID 			=	"aaaa",
+		Author			=	"KiderionLykon",
+		SourceBook		=	"TCoE",
+		MainClasse		=	false,
+		isLvl20         =   true,
+		isOutdated		=	false
+    },
+    {
+		Name 			=	"Artificer - Artillerist",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/1779",
+		modGuid 		=	"aaaa",
+		UUID 			=	"aaaa",
+		Author			=	"KiderionLykon",
+		SourceBook		=	"TCoE",
+		MainClasse		=	false,
+		isLvl20         =   true,
+		isOutdated		=	false
+    },
+    {
+		Name			=	"Artificer - Battle Smith",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/1779",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"KiderionLykon",
+		SourceBook		=	"TCoE",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+    },
+
+-- Barbarian subclasses
+	{
+		Name			=	"Barbarian - Path of the Zealot",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/6411",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"sumradagnoth8",
+		SourceBook		=	"PHB",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"Barbarian - Path of the Ancestral Guardian",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/6411",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"Lexicoo",
+		SourceBook		=	"XGtE",
+		MainClasse		=	false,
+		isLvl20			=   false,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"Barbarian - Path of the Battle Rager",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/5085",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"sumradagnoth8",
+		SourceBook		=	"XGtE",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"Barbarian - Path of the Storm Herald",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/8419",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"sumradagnoth8",
+		SourceBook		=	"XGtE",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"Barbarian - Path of the Storm Herald",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/2996",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"Nitreaux",
+		SourceBook		=	"XGtE",
+		MainClasse		=	false,
+		isLvl20			=   false,
+		isOutdated		=	true
+	},
+	{
+		Name			=	"Barbarian - Path of the Beast",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/6404",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"sumradagnoth8",
+		SourceBook		=	"TCoE",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"Barbarian - Path of the Giant",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/8037",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"sumradagnoth8",
+		SourceBook		=	"BPGotG",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"Barbarian - Path of the Giant",
+		modURL			=	"https://www.nexusmods.com/baldursgate3/mods/1806",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"Sewinlife",
+		SourceBook		=	"BPGotG",
+		MainClasse		=	false,
+		isLvl20			=   false,
+		isOutdated		=	true
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	},
+	{
+		Name			=	"New",
+		modURL			=	"",
+		modGuid			=	"aaaa",
+		UUID			=	"aaaa",
+		Author			=	"aaa",
+		SourceBook		=	"",
+		MainClasse		=	false,
+		isLvl20			=   true,
+		isOutdated		=	false
+	}
+}
