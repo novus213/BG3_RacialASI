7c4421feb960a370ded5f049e17563f2b6f9745e
Novus213 <44229417+novus213@users.noreply.github.com>
2024-07-31 20:25:29 +0200

    deport func to libs

--- RacialASI/Mods/RacialASI/ScriptExtender/Lua/Client/Debug.lua
+++ RacialASI/Mods/RacialASI/ScriptExtender/Lua/Client/Debug.lua
@@ -4,12 +4,14 @@
 local function CheckTableUuid(t1, name, printo)
   for _, uuidIn in pairs(t1) do
     local modDataE = Ext.Mod.GetMod(uuidIn)
-  
+
     if printo == "warn" then
-      BasicWarning(string.format("TYPE %s , modData.Info.Directory : %s , modData.Info.ModuleUUID : %s, modData.Info.Name : %s, modData.Info.Author : %s",
+      BasicWarning(string.format(
+        "TYPE %s , modData.Info.Directory : %s , modData.Info.ModuleUUID : %s, modData.Info.Name : %s, modData.Info.Author : %s",
         name, modDataE.Info.Directory, modDataE.Info.ModuleUUID, modDataE.Info.Name, modDataE.Info.Author))
     else
-      BasicPrint(string.format("TYPE %s , modData.Info.Directory : %s , modData.Info.ModuleUUID : %s, modData.Info.Name : %s, modData.Info.Author : %s",
+      BasicPrint(string.format(
+        "TYPE %s , modData.Info.Directory : %s , modData.Info.ModuleUUID : %s, modData.Info.Name : %s, modData.Info.Author : %s",
         name, modDataE.Info.Directory, modDataE.Info.ModuleUUID, modDataE.Info.Name, modDataE.Info.Author))
     end
   end
@@ -19,8 +21,11 @@ local function compare(t1, t2)
   local tempUuid = {}
   for _, uuidI in pairs(t1) do
     for _, uuidIn in pairs(t2) do
-      if uuidI ~= uuidIn then
-        table.insert(tempUuid, uuidI)
+      local modData1 = Ext.Mod.GetMod(uuidI)
+      local modData2 = Ext.Mod.GetMod(uuidIn)
+
+      if modData1.Info.ModuleUUID ~= modData2.Info.ModuleUUID then
+        table.insert(tempUuid, uuidIn)
       end
     end
   end
@@ -50,6 +55,7 @@ local function CheckLoadOrderUuid(dataB, name)
   InloadOrder = table.unique(InloadOrder)
   noinLoadOrder = table.unique(noinLoadOrder)
   local t1 = compare(InloadOrder, noinLoadOrder)
+  t1 = table.unique(t1)
   CheckTableUuid(InloadOrder, name)
   CheckTableUuid(t1, name, "warn")
 end
