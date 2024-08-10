8076a76a7e8dc71505720e5f79366311ad7cb202
Novus213 <44229417+novus213@users.noreply.github.com>
2024-08-10 16:48:24 +0200

    test classeDescription hidden

--- RacialASI/Mods/RacialASI/ScriptExtender/Lua/Server/EventHandlers.lua
+++ RacialASI/Mods/RacialASI/ScriptExtender/Lua/Server/EventHandlers.lua
@@ -3,8 +3,10 @@ EHandlers = {}
 function EHandlers.SyncVarsCharCS()
   Ext.Osiris.RegisterListener("DB_TUT_CharacterCreation_Started", 4, "before", function ()
     if McmVarsGeneralSettings["RASI"] == true and Ext.IsServer() then
-    -- hide race / subrace / classe ruleSets
-      Ext.Require("Core/Builder5e.lua")
+      -- hide race / subrace / classe ruleSets
+      local res = Ext.StaticData.GetGuidResource("03f972eb-de3c-4cdb-9050-e8e3fa0526eb", "ClassDescription")
+      res.ResourceUUID = "00000000-0000-0000-0000-000000000000"
+        Ext.Require("Core/Builder5e.lua")
       Core.Builder5e()
     end
   end)
