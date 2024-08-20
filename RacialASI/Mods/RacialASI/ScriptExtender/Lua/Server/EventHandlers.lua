0ced744d6599daab62d3a41e2ff4c7a887aa9715
Novus213 <seorndev@gmail.com>
2024-08-20 16:43:02 +0200

    update KL (test merge) and add fix raw 5e from hobgoblin

--- RacialASI/Mods/RacialASI/ScriptExtender/Lua/Server/EventHandlers.lua
+++ RacialASI/Mods/RacialASI/ScriptExtender/Lua/Server/EventHandlers.lua
@@ -13,6 +13,14 @@ end
 function EHandlers.SyncVarsLLGame()
   Ext.Osiris.RegisterListener("DB_LevelGameplayLoadedOnce_WaitForGameplay", 4, "before", function ()
     if McmVarsGeneralSettings["RASI"] == true and Ext.IsServer() then
+      if Ext.Mod.IsModLoaded(Data.Deps.Framework_FM_GUID.ModuleUUID) and Ext.Mod.IsModLoaded(Data.Deps.VariantHuman_FM_GUID.ModuleUUID) then
+        local Wyll = "efc9d114-0296-4a30-b701-365fc07d44fb"
+        local Gale = "35c3caad-5543-4593-be75-e7deba30f062" 
+        local resW = Ext.StaticData.Get(Wyll, "Origin")
+        local resG = Ext.StaticData.Get(Gale, "Origin")
+        resW.RaceUUID = "db0262f7-8c73-43b5-ad69-7451089ff1a1" --Human default Fantastic M RACEUUID
+        resG.RaceUUID = "db0262f7-8c73-43b5-ad69-7451089ff1a1" --Human default Fantastic M RACEUUID
+      end    
       Ext.Require("Core/Builder5e.lua")
       Core.Builder5e()
     end
